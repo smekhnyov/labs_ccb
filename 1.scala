5c679472bcdf3a337a5e7eb8a9170bd4da662bb5
Ivan Smekhnyov <156292095+smekhnyov@users.noreply.github.com>
2024-04-22 12:28:37 +0300

    Update 1.scala

--- 1.scala
+++ 1.scala
@@ -1,22 +1,16 @@
 import org.apache.spark.sql.SparkSession
-val rdd = sc.textFile("/opt/spark/smekhnev.txt")
+val rdd = sc.textFile("/opt/spark/smekhnyov.txt")
 
-val targetWord = "line"
+val targetWord = "file"
 val wordCount = rdd.filter(_.toLowerCase.contains(targetWord)).count()
 println(s"Количество вхождений слова '$targetWord' в файле: $wordCount")
 
-val wordsRDD = rdd.flatMap(_.split("\\s+")).filter(_.nonEmpty)
+val rdd = rdd.flatMap(_.split("\\s+")).filter(_.nonEmpty).collect
 
-//Целочисленный массив
 val intArrayRDD = sc.parallelize(Array(1,2,3,4,5))
-//Применение reduce для нахождения суммы всех элементов
 val reduceRDD = intArrayRDD.reduce(_+_)
-//Применение map для умножения каждого элемента на 2
-val mapRDD = intArrayRDD.map(_*2)
+val mapRDD = intArrayRDD.map(_*2).collect
 
-//Ассоциативный массив
 val associativeArrayRDD = sc.parallelize(Array(("a", 1), ("b", 2), ("c", 3), ("a", 4)))
-//Применение reduceByKey для сложения значений по ключу
 val reduceByKeyRDD = associativeArrayRDD.reduceByKey((x, y) => x + y)
-//Применение mapValues для умножения каждого элемента на 2
-val mapValuesRDD = associativeArrayRDD.mapValues(_*2)
+val mapValuesRDD = associativeArrayRDD.mapValues(_*2).collect
