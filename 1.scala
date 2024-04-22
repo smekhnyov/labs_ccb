544b5ef7724928618073d6ef303a4af795d4bfa5
Ivan Smekhnyov <156292095+smekhnyov@users.noreply.github.com>
2024-04-22 11:42:16 +0300

    Update 1.scala

--- 1.scala
+++ 1.scala
@@ -6,10 +6,17 @@ val wordCount = rdd.filter(_.toLowerCase.contains(targetWord)).count()
 println(s"Количество вхождений слова '$targetWord' в файле: $wordCount")
 
 val wordsRDD = rdd.flatMap(_.split("\\s+")).filter(_.nonEmpty)
-val integerRDD = wordsRDD.map(word => (word.length, 1)).reduceByKey(_+_).sortByKey()
-val associativeRDD = wordsRDD.map(word => (word, 1)).reduceByKey(_+_)
-integerRDD.collect().foreach(println)
-associativeRDD.collect().foreach(println)
 
-val wordLengthsRDD = wordsRDD.map(_.length)
-val totalWords = wordLengthsRDD.reduce(_ + _)
+//Целочисленный массив
+val intArrayRDD = sc.parallelize(Array(1,2,3,4,5))
+//Применение reduce для нахождения суммы всех элементов
+val reduceRDD = intArrayRDD.reduce(_+_)
+//Применение map для умножения каждого элемента на 2
+val mapRDD = intArrayRDD.map(_*2)
+
+//Ассоциативный массив
+val associativeArrayRDD = sc.parallelize(Array(("a", 1), ("b", 2), ("c", 3), ("a", 4)))
+//Применение reduceByKey для сложения значений по ключу
+val reduceByKeyRDD = associativeArrayRDD.reduceByKey((x, y) => x + y)
+//Применение mapValues для умножения каждого элемента на 2
+val mapValuesRDD = associativeArrayRDD.mapValues(_*2)
